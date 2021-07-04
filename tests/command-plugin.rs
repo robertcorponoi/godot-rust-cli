077d398e8341a2b4a8ae19dd3fcfdeac66fd6fa2
Robert Corponoi <robertcorponoi@gmail.com>
2021-07-03 21:14:15 -0500

    Added cli-version to configuration

--- tests/command-plugin.rs
+++ tests/command-plugin.rs
@@ -37,6 +37,10 @@ fn plugin_create_library_structure() -> Result<(), Box<dyn Error>> {
     let config = read_to_string("godot-rust-cli.json")?;
     let config_json: Value = serde_json::from_str(&config)?;
     assert_eq!(config_json["name"], "Directory Browser");
+    assert_eq!(
+        config_json["cli_version"],
+        env!("CARGO_PKG_VERSION").to_string()
+    );
     assert_eq!(config_json["godot_project_dir_name"], "platformer");
     assert_eq!(config_json["is_plugin"], true);
     assert_eq!(config_json["modules"], json!([]));
